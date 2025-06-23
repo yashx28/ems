d6d502c258ca134acdfb6516a198095d1d3c07fc
yash <yashbhtaia81989@gmail.com>
2025-06-23 12:51:52 +0530

    Updated homepage layout and fixed bug

--- src/component/Auth/Login.jsx
+++ src/component/Auth/Login.jsx
@@ -1,6 +1,6 @@
   import React from 'react'
   import {useState } from 'react';
-  import { setUserData } from '../../utils/localstorage'; 
+import { setUserData } from '../../utils/localstorage';
   import axios from 'axios'
   import { useNavigate } from 'react-router-dom'
   const Login = () => {
